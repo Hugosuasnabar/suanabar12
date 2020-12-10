f4b63e0675f46de53f1e688fab2f14fd73cb992b
Hugosuasnabar <75793641+Hugosuasnabar@users.noreply.github.com>
2020-12-10 09:28:27 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+//DESARROLLOAR EL CODIGO DE LOS EJERCICIOS PROPUESTOS MEDIANTE
+//FUNCIONES
+
+//desarrollo 01
+function mostrar(){
+    alert("demostracin de llamar a una funcion");
+}
+
+
+
+
+
+//desarrollo 02
+
+
+
+
+
+
+//desarrollo 03
